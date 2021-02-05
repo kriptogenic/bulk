41c8c3fe84e7337552a5fae7d8b69e9a7bd96844
kriptonuz <kripton253@gmail.com>
2021-02-05 15:39:08 +0200

    var

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 
 
-echo "Hello world";
+//echo "Hello world";
 
 $p = $_GET['sa'] ?? null;
 
 $redis = new Redis();
 
-$redis->connect(getenv('REDIS_URL'));
+var_dump($redis->connect(getenv('REDIS_URL')));
 
 var_dump($redis->set('key', $p));
