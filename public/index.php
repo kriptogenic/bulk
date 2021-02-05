1833460588694027bb3c98277079636a6eb13a82
kriptonuz <kripton253@gmail.com>
2021-02-05 15:47:29 +0200

    redis auth

--- public/index.php
+++ public/index.php
@@ -7,6 +7,10 @@ $p = $_GET['sa'] ?? null;
 
 $redis = new Redis();
 
-var_dump($redis->connect(getenv('REDIS_URL')));
+$redis_conf = parse_url(getenv('REDIS_URL'));
+
+var_dump($redis->connect($redis_conf['host'], $redis_conf['port']));
+
+$redis->auth($redis_conf['pass']);
 
 var_dump($redis->set('key', $p));
