5c992b9af8ea278ddc7020a1e094fb5e3817ab43
kriptonuz <kripton253@gmail.com>
2021-02-05 15:48:11 +0200

    redis auth

--- public/get.php
+++ public/get.php
@@ -2,6 +2,10 @@
 
 $redis = new Redis();
 
-$redis->connect(getenv('REDIS_URL'));
+$redis_conf = parse_url(getenv('REDIS_URL'));
+
+$redis->connect($redis_conf['host'], $redis_conf['port']);
+
+$redis->auth($redis_conf['pass']);
 
 echo $redis->get('key');
