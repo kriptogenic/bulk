10a3af379eb101c44491f34692d7d8cc80207756
kriptonuz <kripton253@gmail.com>
2021-02-21 00:03:48 +0200

    rebuild

--- bin/server.php
+++ bin/server.php
@@ -5,7 +5,6 @@ $http = new Swoole\HTTP\Server("0.0.0.0", getenv('PORT'));
 $http->on('request', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
     $data = json_decode($request->getContent());
     if ($data->chat_id % 3) {
-
         $response->setStatusCode(429);
     }
 //    echo $request->getData();
