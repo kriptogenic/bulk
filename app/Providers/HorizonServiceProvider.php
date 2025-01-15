600c44e85407337a6b7d5d9a8f0a15ecde7ac2bc
Azizbek Ochilov <azizbek.och@tbcbank.uz>
2025-01-15 12:26:23 +0500

    horizon auth with moonshine

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
+use MoonShine\Laravel\MoonShineAuth;
 
 class HorizonServiceProvider extends HorizonApplicationServiceProvider
 {
@@ -28,7 +29,11 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function (?User $user) {
+        Gate::define('viewHorizon', function (?User $user): bool {
+            $user ??= MoonShineAuth::getModel();
+            if ($user === null) {
+                return false;
+            }
             return $user->isSuperAdmin();
         });
     }
